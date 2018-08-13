d73c896431dc120736f5fcd6ae3c1db80c32dc2b
Manuel <manuelpazrobles@gmail.com>
2018-08-12 22:01:31 -0500

    Se agregaron los objetos para hacer la referencia de los controles que se tienen en la vista activityMain los controles de tipo Edit Text de tal manera que se le pueden asignar funciones y poder obtener datos de estos controles tambien se agregaron dos variables uno de tipo de tipo String y una de tipo entero.

--- app/src/main/java/com/example/manuel/cursokotlinandroid/MainActivity.kt
+++ app/src/main/java/com/example/manuel/cursokotlinandroid/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.manuel.cursokotlinandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-
+    private var editName: EditText? = null
+    private var editAge: EditText? = null
+    private var name: String? = null
+    private var age = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editName = findViewById(R.id.editText_Name)
+        editAge = findViewById(R.id.editText_Age)
     }
 }
