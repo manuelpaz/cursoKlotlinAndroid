0dbb8216023bc093b9b40065837947c2955f4abf
Manuel <manuelpazrobles@gmail.com>
2018-08-13 20:55:34 -0500

    Se agregaron eventos los controles editName y editAge esto a su vez creo una interfaz que implementa funciones que se ejecutan al haber cambios en los controles, de las funciones que se implementa esta una que se ejecutan al estar introduciendo caracteres en los controles y dentro de esta función se agrego un objeto Toast que muestra los caracteres mientras se van introduciendo.

--- app/src/main/java/com/example/manuel/cursokotlinandroid/MainActivity.kt
+++ app/src/main/java/com/example/manuel/cursokotlinandroid/MainActivity.kt
@@ -2,9 +2,24 @@ package com.example.manuel.cursokotlinandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextWatcher {
+    override fun afterTextChanged(p0: Editable?) {
+
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        Toast.makeText(this, p0.toString(), Toast.LENGTH_SHORT).show()
+    }
+
     private var editName: EditText? = null
     private var editAge: EditText? = null
     private var name: String? = null
@@ -15,5 +30,8 @@ class MainActivity : AppCompatActivity() {
 
         editName = findViewById(R.id.editText_Name)
         editAge = findViewById(R.id.editText_Age)
+
+        editName?.addTextChangedListener(this)
+        editAge?.addTextChangedListener(this)
     }
 }
