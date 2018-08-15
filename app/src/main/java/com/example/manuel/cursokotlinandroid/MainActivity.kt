996c20ff689727e5c0b9fe4176168f7460eeb95b
Manuel <manuelpazrobles@gmail.com>
2018-08-15 00:29:31 -0500

    Se da funcionalidad a los controles creados anteriormente haciendo que se muestre los valores introducidos en los Edit Text se visualicen en los Text view creados.

--- app/src/main/java/com/example/manuel/cursokotlinandroid/MainActivity.kt
+++ app/src/main/java/com/example/manuel/cursokotlinandroid/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), TextWatcher {
     override fun afterTextChanged(p0: Editable?) {
@@ -17,11 +19,19 @@ class MainActivity : AppCompatActivity(), TextWatcher {
     }
 
     override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        Toast.makeText(this, p0.toString(), Toast.LENGTH_SHORT).show()
+        name = editName?.text.toString()
+        age = Integer.valueOf(editAge?.text.toString())
+
+        texName?.text= name
+        textAge?.text = age.toString()
+        //Toast.makeText(this, p0.toString(), Toast.LENGTH_SHORT).show()
     }
 
     private var editName: EditText? = null
     private var editAge: EditText? = null
+    private var texName: TextView? = null
+    private var textAge: TextView? = null
+
     private var name: String? = null
     private var age = 0
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,8 +40,10 @@ class MainActivity : AppCompatActivity(), TextWatcher {
 
         editName = findViewById(R.id.editText_Name)
         editAge = findViewById(R.id.editText_Age)
+        texName = findViewById(R.id.textView_Name)
+        textAge = findViewById(R.id.textView_Age)
 
-        editName?.addTextChangedListener(this)
-        editAge?.addTextChangedListener(this)
+        editName!!.addTextChangedListener(this)
+        editAge!!.addTextChangedListener(this)
     }
 }
