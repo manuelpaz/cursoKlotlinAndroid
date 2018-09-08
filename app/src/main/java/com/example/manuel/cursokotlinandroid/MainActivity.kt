4bc1161632af1756e754c093ad5db79b1cb67348
Manuel <manuelpazrobles@gmail.com>
2018-09-07 21:05:47 -0500

    En esta versión se utiliza la sobrecarga de operadores aritmeticos que pueden ser utilizados en vez de los operadores que normalmente utilizamos por ejemplo en vez de usar la siguiente expresión valor1 + valor2 se puede utilizar valor1.plus(valor2) dando el mismo resultado (claro habiendo declarado las variables y asignado valores)

--- app/src/main/java/com/example/manuel/cursokotlinandroid/MainActivity.kt
+++ app/src/main/java/com/example/manuel/cursokotlinandroid/MainActivity.kt
@@ -30,21 +30,25 @@ class MainActivity : AppCompatActivity(), TextWatcher, View.OnClickListener {
 
     private fun operacion(){
 
-        var cadena = "MKJUO"
         var entero = 0
-        /*var doble: Double = 5.0
-        var valor: Boolean = true
-        var char: Char = 'k'
-        var f: Float = 5.2f*/
+        val valor1 = 5
+        val valor2 = 6
 
-        if(false){
-            entero = 5*5
-        }else{
-            cadena = "no tiene permiso"
-        }
+        entero = valor1 + valor2
+        entero = valor1.plus(valor2)
 
-        val d = null ?: 4 //El operador elvis ?: evalua si al lado izquierdo, la epresión devuelve un null asignando a la variable el valor que se encuentra al lado derecho en este caso se asignara 4 ya que se tiene null al lado izquierdo en caso contrario se asignara a la variable el valor resultado de la expresión al lado izquierdo del operador elvis
-        val e = cadena.length ?: 4 //En este caso a la variable e se le asignara el valor de 5 ya que la expresión a su lado izquierdo no es null y cadena.length devuelve 5
+        entero = valor1 - valor2
+        entero = valor1.minus(valor2)
+
+        entero = valor1*valor2
+        entero = valor1.times(valor2)
+
+        entero = valor1/valor2
+        entero = valor2.div(valor1)
+
+        entero = valor2%valor1
+        entero = valor2.rem(valor1)
+
         /*name = editName?.text.toString()
         age = editAge?.text.toString()
         if(name != "") textName?.text = name
