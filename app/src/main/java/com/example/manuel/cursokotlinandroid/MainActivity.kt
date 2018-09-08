967fcbff0c102969e27106123a729de0e1095fc9
Manuel <manuelpazrobles@gmail.com>
2018-09-07 19:47:48 -0500

    Se muestra el uso del operador elvis ?: el cual es parecido a la sentencia if else solo que el operador elvis evalua null esto es que si la expresión de la izquierda es null o devuelve null se asignara a la variable declarada el valor que se encuentra a la derecha del operador elvis y en caso de no ser null la expresión a la izquierda del operador se asignara el valor que haya o que devuelva la expresión

--- app/src/main/java/com/example/manuel/cursokotlinandroid/MainActivity.kt
+++ app/src/main/java/com/example/manuel/cursokotlinandroid/MainActivity.kt
@@ -30,13 +30,12 @@ class MainActivity : AppCompatActivity(), TextWatcher, View.OnClickListener {
 
     private fun operacion(){
 
-        var cadena = ""
+        var cadena = "MKJUO"
         var entero = 0
         /*var doble: Double = 5.0
         var valor: Boolean = true
         var char: Char = 'k'
         var f: Float = 5.2f*/
-        val d = 4 //esto es una variable inmutable lo que quiere decir que no se le puede cambiar el valor
 
         if(false){
             entero = 5*5
@@ -44,7 +43,8 @@ class MainActivity : AppCompatActivity(), TextWatcher, View.OnClickListener {
             cadena = "no tiene permiso"
         }
 
-
+        val d = null ?: 4 //El operador elvis ?: evalua si al lado izquierdo, la epresión devuelve un null asignando a la variable el valor que se encuentra al lado derecho en este caso se asignara 4 ya que se tiene null al lado izquierdo en caso contrario se asignara a la variable el valor resultado de la expresión al lado izquierdo del operador elvis
+        val e = cadena.length ?: 4 //En este caso a la variable e se le asignara el valor de 5 ya que la expresión a su lado izquierdo no es null y cadena.length devuelve 5
         /*name = editName?.text.toString()
         age = editAge?.text.toString()
         if(name != "") textName?.text = name
